e22df60a4e36c64386c1132760b2a370a8e87e5e
urielsnt <urielsantiagome04@gmail.com>
2026-05-19 22:10:37 -0600

    fix: agregar iconos provisionales y configurarlos para habilitar instalacion PWA

--- logistica-pwa/vite.config.js
+++ logistica-pwa/vite.config.js
@@ -23,14 +23,15 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
